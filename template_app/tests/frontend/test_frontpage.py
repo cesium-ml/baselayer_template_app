d72735c9452ce284379847f0ec30916ee2171f7c
Stefan van der Walt <stefanv@berkeley.edu>
2017-11-28 17:02:03 -0800

    Fix xpaths for frontend tests

--- template_app/tests/frontend/test_frontpage.py
+++ template_app/tests/frontend/test_frontpage.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def test_front_page(driver):
     driver.get("/")
     assert 'localhost' in driver.current_url
-    button = driver.wait_for_xpath("//div[contains(@title,'connected')]")
-    link = driver.find_element_by_partial_link_text('Click here')
-    link.click()
-    driver.wait_for_xpath("//div[contains(text(),'Hello')]")
+    driver.wait_for_xpath("//div[contains(@title,'connected')]")
+    button = driver.wait_for_xpath("//button[contains(text(), 'Click here')]")
+    button.click()
+    driver.wait_for_xpath("//div[contains(text(),'Hello from Baselayer')]")
