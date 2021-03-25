2d1fb7d699fbe01ffb6d7704c9c3249e3f5fe42b
kshin <kshin@caltech.edu>
2021-03-25 15:32:22 -0400

    Remove debug code and tests from other branch

--- template_app/tests/conftest.py
+++ template_app/tests/conftest.py
@@ -3,7 +3,6 @@
 import pytest
 import os
 import pathlib
-from datetime import datetime
 import distutils.spawn
 import types
 import shutil
@@ -22,38 +21,3 @@ print('Loading test configuration from test_config.yaml')
 basedir = pathlib.Path(os.path.dirname(__file__))/'../..'
 cfg = load_config([basedir/'test_config.yaml'])
 set_server_url(f'http://localhost:{cfg["ports.app"]}')
-
-# set up a hook to be able to check if a test has failed
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # execute all other hooks to obtain the report object
-
-    outcome = yield
-    rep = outcome.get_result()
-
-    # set a report attribute for each phase of a call, which can
-    # be "setup", "call", "teardown"
-
-    setattr(item, "rep_" + rep.when, rep)
-
-# check if a test has failed
-@pytest.fixture(scope="function", autouse=True)
-def test_failed_check(request):
-    yield
-    if request.node.rep_call.failed and 'driver' in request.node.funcargs:
-        webdriver = request.node.funcargs['driver']
-        take_screenshot_and_page_source(webdriver, request.node.nodeid)
-
-
-
-# make a screenshot with a name of the test, date and time.
-# also save the page HTML.
-def take_screenshot_and_page_source(webdriver, nodeid):
-    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace(
-        "/", "_"
-    ).replace(":", "_")
-    file_name = os.path.join(os.path.dirname(__file__), '../../test-results', file_name)
-    pathlib.Path(file_name).parent.mkdir(parents=True, exist_ok=True)
-    webdriver.save_screenshot(file_name)
-    with open(file_name.replace('png', 'html'), 'w') as f:
-        f.write(webdriver.page_source)
