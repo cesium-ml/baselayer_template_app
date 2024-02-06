ec79023f2770d907c78eea9b8dddee22110a43ff
Theodlz <theophile.dulaz@gmail.com>
2024-02-06 14:43:37 -0800

    test that brotli is supported as expected

---
+++ template_app/tests/frontend/test_brotli.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+import requests
+
+from baselayer.app.config import load_config as _load_config
+
+print("Loading test configuration from test_config.yaml")
+basedir = pathlib.Path(os.path.dirname(__file__)) / "../.."
+cfg = _load_config([basedir / "test_config.yaml"])
+server_url = f'http://localhost:{cfg["ports.app"]}'
+
+def test_brotli():
+    # Test that the server is using Brotli compression
+    # when requested by the client
+    r = requests.get(server_url, headers={"Accept-Encoding": "br"})
+    assert r.status_code == 200
+    assert r.headers.get("Content-Encoding") == "br"
