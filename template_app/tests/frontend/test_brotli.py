a35b621ef619bd5bae90d8828c1467577218a26d
Theodlz <theophile.dulaz@gmail.com>
2024-02-10 11:44:39 -0800

    remove pytest if statement

--- template_app/tests/frontend/test_brotli.py
+++ template_app/tests/frontend/test_brotli.py
@@ -9,9 +9,7 @@ from baselayer.tools.fill_conf_values import nginx_brotli_installed
 basedir = pathlib.Path(os.path.dirname(__file__)) / "../../.."
 cfg = _load_config([basedir / "test_config.yaml"])
 server_url = f'http://localhost:{cfg["ports.app"]}'
-installed, _, _ = nginx_brotli_installed()
 
-@pytest.mark.skipif(not installed, reason="Brotli not installed")
 def test_brotli():
     # Test that the server is using Brotli compression
     # when requested by the client
