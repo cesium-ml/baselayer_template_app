1a483bb93fdd24ce928af2fdb3fb8480ea71ceba
Theodlz <theophile.dulaz@gmail.com>
2024-02-09 17:37:24 -0800

    pin new baselayer, skip test if brotli not installed on machine

--- template_app/tests/frontend/test_brotli.py
+++ template_app/tests/frontend/test_brotli.py
@@ -1,18 +1,20 @@
 import os
 import pathlib
 import requests
+import pytest
 
 from baselayer.app.config import load_config as _load_config
+from baselayer.tools.fill_conf_values import nginx_brotli_installed
 
-print("Loading test configuration from test_config.yaml")
 basedir = pathlib.Path(os.path.dirname(__file__)) / "../../.."
 cfg = _load_config([basedir / "test_config.yaml"])
 server_url = f'http://localhost:{cfg["ports.app"]}'
+installed, _, _ = nginx_brotli_installed()
 
+@pytest.mark.skipif(not installed, reason="Brotli not installed")
 def test_brotli():
     # Test that the server is using Brotli compression
     # when requested by the client
     r = requests.get(server_url, headers={"Accept-Encoding": "br"})
-    print(r.headers)
     assert r.status_code == 200
     assert r.headers.get("Content-Encoding") == "br"
