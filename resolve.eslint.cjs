5c16d9c5551563ee58973ed9449ba5abc3fb1096
Stefan van der Walt <stefanv@berkeley.edu>
2025-09-19 21:15:48 -0700

    Simplify resolver

--- resolve.eslint.cjs
+++ resolve.eslint.cjs
@@ -12,13 +12,5 @@ module.exports = {
     alias: {
       baselayer: path.resolve(ROOT, "baselayer/static/js"),
     },
-
-    // Optional: help resolution behavior be closer to bundler defaults
-    mainFields: ["browser", "module", "main"],
-    mainFiles: ["index"],
-    conditionNames: ["import", "module", "browser", "default"],
-    symlinks: true,
-    // Optional: let absolute imports from src work if desired
-    // modules: [r('src'), 'node_modules'],
   },
 };
